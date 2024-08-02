067d556c7c1222dc68dcbc968ae4ac44ac2b1084
dimzka <dimas9aul2@gmail.com>
2024-08-02 20:15:30 +0700

    Singleton

--- Singleton/Main.java
+++ Singleton/Main.java
@@ -1,25 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        // Get the single instance of RepairShop
         RepairShop shop = RepairShop.getInstance();
 
-        // Create some motorcycles
         Motorcycle bike1 = new Motorcycle("Yamaha R15");
         Motorcycle bike2 = new Motorcycle("Honda CBR");
 
-        // Log repairs
-        shop.logRepair(bike1, "Engine oil change");
-        shop.logRepair(bike2, "Brake pad replacement");
+        shop.logRepair(bike1, "Oli mesin diganti");
+        shop.logRepair(bike2, "Kampas rem diganti");
 
-        // Print the current state of the repair shop
         shop.printCurrentState();
 
-        // Demonstrating singleton behavior
         RepairShop anotherShopInstance = RepairShop.getInstance();
         if (shop == anotherShopInstance) {
-            System.out.println("Both instances are the same. Singleton pattern works!");
+            System.out.println("cek singleton : sukses!");
         } else {
-            System.out.println("Different instances. Singleton pattern failed!");
+            System.out.println("cek singleton : gagal!");
         }
     }
 }
--- Singleton/RepairShop.java
+++ Singleton/RepairShop.java
@@ -1,12 +1,9 @@
 public class RepairShop {
-    // Static variable to hold the single instance
     private static RepairShop instance;
 
-    // Private constructor to prevent instantiation from outside
     private RepairShop() {
     }
 
-    // Method to get the single instance of RepairShop
     public static RepairShop getInstance() {
         if (instance == null) {
             instance = new RepairShop();
@@ -14,13 +11,11 @@ public class RepairShop {
         return instance;
     }
 
-    // Log repair details
     public void logRepair(Motorcycle motorcycle, String repairDetails) {
         System.out.println("Repairing motorcycle: " + motorcycle.getModel());
         System.out.println("Repair details: " + repairDetails);
     }
 
-    // Print current state
     public void printCurrentState() {
         System.out.println("RepairShop is operational.");
     }
